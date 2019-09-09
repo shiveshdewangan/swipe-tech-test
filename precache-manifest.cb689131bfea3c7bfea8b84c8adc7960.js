baa2956a2378d6060355cc8c73c8d3729da18bc2
Shivesh Dewangan <shivesh.dewangan@macquarie.com>
2019-09-09 11:26:26 +1000

    Updates

---
+++ precache-manifest.cb689131bfea3c7bfea8b84c8adc7960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3d0c09245457201256a44bf9b5faeb",
+    "url": "/swipe-tech-test/index.html"
+  },
+  {
+    "revision": "fd6834aee0fbdab019ed",
+    "url": "/swipe-tech-test/static/css/main.ad389ffa.chunk.css"
+  },
+  {
+    "revision": "9ead7ef518535dde8498",
+    "url": "/swipe-tech-test/static/js/2.04ca56f4.chunk.js"
+  },
+  {
+    "revision": "fd6834aee0fbdab019ed",
+    "url": "/swipe-tech-test/static/js/main.85cedfb6.chunk.js"
+  },
+  {
+    "revision": "729aa20b94c21fa2e15c",
+    "url": "/swipe-tech-test/static/js/runtime~main.6ca42072.js"
+  }
+]);
